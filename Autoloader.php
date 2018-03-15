c3e7008fdd9299b33e82898413b2d2daa43a3826
Myriam <lieninformatique9@gmail.com>
2018-03-15 20:16:06 +0100

    add function register in Autoloader.php

--- Autoloader.php
+++ Autoloader.php
@@ -1,8 +1,13 @@
 <?php
 class Autoloader
 {
+    static function register()
+    {
+        spl_autoload_register(array(__class__, 'autoload'));
+    }
+
     static function autoload($class_name)
     {
-        require 'class/' . $class_name . '.php';
+        require $class_name . '.php';
     }
 }
--- Personnage.php
+++ Personnage.php
@@ -6,6 +6,57 @@ class Personnage
     private $atk = 20;
     private $nom;
 
+    public function __construct($nom)
+    {
+        $this->nom = $nom;
+    }
+    
+    public function getVie()
+    {
+        return $this->vie;
+    }
+    
+    public function getAtk()
+    {
+        return $this->atk;
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function crier()
+    {
+        echo 'à l\'attaque !';
+    }
+
+    public function regenerer($vie = null)
+    {
+        if(is_null($vie))
+        {
+            $this->vie = 100;
+        }
+        else
+        {
+            $this->vie += $vie;
+        }
+    }
+
+   
+    public function mort()
+    {
+        return $this->vie <= 0;
+    }
+
+    public function attaque($cible)
+    {
+        $cible->vie -= $this->atk;
+        var_dump($cible);
+    }
    
 }
